c2efef0062530cf86dee3187e8d6d0502deac92d
Erick <38795106+Birunda3000@users.noreply.github.com>
2023-04-12 20:43:45 +0000

    adding models

--- models/tag_model.py
+++ models/tag_model.py
@@ -4,4 +4,89 @@ class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     description = db.Column(db.String(500), unique=False, nullable=True)
+
+    def __repr__(self):
+        return "<Tag %r>" % self.name
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    def __repr__(self):
+        return "<Image %r>" % self.id
+
+class Life(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    popular_name = db.Column(db.String(80), unique=False, nullable=True)
+    description = db.Column(db.String(500), unique=False, nullable=True)
+    origin = db.Column(db.Integer, unique=False, nullable=False)
+    extinction = db.Column(db.Integer, unique=False, nullable=True)
+    individuals_number = db.Column(db.Integer, unique=False, nullable=True)
+    # Relationships
+    #ancestors = many to many with same table
+    #images = one to many with images table
+    #tags = many to many with tags table
+    # Opcional fields
+    #fossils?
+    #sites - actual - origin
+    def __repr__(self):
+        return "<Life %r>" % self.name
+
+class Domain(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    popular_name = db.Column(db.String(80), unique=False, nullable=True)
+    description = db.Column(db.String(500), unique=False, nullable=True)
+    origin = db.Column(db.Integer, unique=False, nullable=False)
+    extinction = db.Column(db.Integer, unique=False, nullable=True)
+    individuals_number = db.Column(db.Integer, unique=False, nullable=True)
+    # Relationships
+    #ancestors = many to many with same table
+    #images = one to many with images table
+    #tags = many to many with tags table
+    #life = one to many with life table
+    # Opcional fields
+    #fossils?
+    #sites - actual - origin
+    def __repr__(self):
+        return "<Domain %r>" % self.name
+
+class Kingdom(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    popular_name = db.Column(db.String(80), unique=False, nullable=True)
+    description = db.Column(db.String(500), unique=False, nullable=True)
+    origin = db.Column(db.Integer, unique=False, nullable=False)
+    extinction = db.Column(db.Integer, unique=False, nullable=True)
+    individuals_number = db.Column(db.Integer, unique=False, nullable=True)
+    # Relationships
+    #ancestors = many to many with same table
+    #images = one to many with images table
+    #tags = many to many with tags table
+    #domain = one to many with life table
+    # Opcional fields
+    #fossils?
+    #sites - actual - origin
+    def __repr__(self):
+        return "<Kingdom %r>" % self.name
+
+class Phylum(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    popular_name = db.Column(db.String(80), unique=False, nullable=True)
+    description = db.Column(db.String(500), unique=False, nullable=True)
+    origin = db.Column(db.Integer, unique=False, nullable=False)
+    extinction = db.Column(db.Integer, unique=False, nullable=True)
+    individuals_number = db.Column(db.Integer, unique=False, nullable=True)
+    # Relationships
+    #ancestors = many to many with same table
+    #images = one to many with images table
+    #tags = many to many with tags table
+    #kingdom = one to many with life table
+    # Opcional fields
+    #fossils?
+    #sites - actual - origin
+    def __repr__(self):
+        return "<Phylum %r>" % self.name
+
+
     
