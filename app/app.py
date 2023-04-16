32d961d68ef90ef7f493642b3bf8e25781a04127
Erick <esdn66@hotmail.com>
2023-04-16 10:49:25 -0300

    tag crud added, adding tests
    -Add tests

--- app/app.py
+++ app/app.py
@@ -1,22 +1,31 @@
 import os.path
-from flask import Flask
 from flask_restx import Api
+from flask import Flask
 from flask_restx.swagger import Swagger
 from flask_sqlalchemy import SQLAlchemy
+from config import db
 from controllers import tag_ns
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "storage.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
 
 api = Api(app, version="1.0", title="Bio Tree API", description="A mini Bio Tree API")
 api.add_namespace(tag_ns, path="/tag")
 
+
+def create_app(config_name: str) -> Flask:
+    app.config.from_object(config_by_name[config_name])
+    db.init_app(app)
+
+    return app
+
+
 if __name__ == "__main__":
     with app.app_context():
         print("Creating database...")
+        db.init_app(app)
         db.drop_all()
         db.create_all()
         db.session.commit()
--- app/controllers/tag_controller.py
+++ app/controllers/tag_controller.py
@@ -1,20 +1,57 @@
+from flask import request
+from flask_restx import Namespace, Resource, fields
 from service.tag_service import *
-from flask_restx import Resource, Namespace, fields
 
-tag_ns = Namespace("tag", description="Tag operations")
+class TagDto:
+    api = Namespace("tag", description="tag related operations")
 
-tag_DTO = tag_ns.model('Tag', {
-    'id': fields.Integer(readOnly=True, description='The tag unique identifier'),
-    'name': fields.String(required=True, description='The tag name'),
-    'description': fields.String(required=False, description='The tag description')
-})
+    tag_post = api.model(
+        "tag_create",
+        {
+            "name": fields.String(required=True, description="tag name"),
+            "description": fields.String(required=False, description="tag description"),
+        },
+    )
 
-@tag_ns.route("/")
-class TagList(Resource):
-    @tag_ns.doc('list_tags')
-    @tag_ns.marshal_list_with(tag_DTO)
+    tag_get = api.clone(
+        "tag_get", tag_post, {"id": fields.Integer(required=True, description="tag id")}
+    )
+
+api = TagDto.api
+tag_ns = api
+
+
+@api.route("")
+class Tag(Resource):
+    @api.doc("list_tags",)
+    @api.marshal_list_with(TagDto.tag_get, code=200, description="Get all tags")
     def get(self):
         """List all tags"""
         return get_all_tags()
 
-#att
+    @api.doc("create_tag")
+    @api.expect(TagDto.tag_post)
+    def post(self):
+        """Create a new tag"""
+        data = request.json
+        return save_new_tag(data=data)
+
+@api.route("/<int:tag_id>")
+class TagById(Resource):
+    @api.doc("get_tag",)
+    @api.marshal_with(TagDto.tag_get, code=200, description="Get tag by id")
+    def get(self, tag_id):
+        """Get tag by id"""
+        return get_tag_by_id(tag_id)
+
+    @api.doc("update_tag")
+    @api.expect(TagDto.tag_post)
+    def put(self, tag_id):
+        """Update tag by id"""
+        data = request.json
+        return update_tag_by_id(tag_id, data=data)
+
+    @api.doc("delete_tag")
+    def delete(self, tag_id):
+        """Delete tag by id"""
+        return delete_tag_by_id(tag_id)
