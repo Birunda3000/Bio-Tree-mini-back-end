5958f5ef369f955e1235bcc8c9962df38f99a11a
Erick <esdn66@hotmail.com>
2023-04-16 07:49:48 -0300

    Circular import error solved
    -Tag controller

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,5 +1,7 @@
 from .tag_controller import *
-'''import sys
+'''
+import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))) #should solve the problem of relative and circular imports, permits not pass through app.py'''
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))) #should solve the problem of relative imports (and circular imports, permits not pass through app.py)
+'''
--- app/controllers/tag_controller.py
+++ app/controllers/tag_controller.py
@@ -1,5 +1,9 @@
-from app.service.tag_service import * # 
+#from app.service.tag_service import * # ImportError: cannot import name 'tag_ns' from partially initialized module 'controllers' (most likely due to a circular import) (C:\Users\esdn6\Documents\GitHub\Bio-Tree-mini-back-end\app\controllers\__init__.py)
+
 #from ..service.tag_service import * #ImportError: attempted relative import beyond top-level package
+
+
+from service.tag_service import *
 from flask_restx import Resource, Namespace, fields
 
 tag_ns = Namespace("tag", description="Tag operations")
